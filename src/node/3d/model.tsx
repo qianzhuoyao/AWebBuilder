68fde702d54ace21c87014ebbe0da74b340c4629
QaeJey <1059116864@qq.com>
2024-05-28 14:50:26 +0800

    feat:assets from server

--- src/node/3d/model.tsx
+++ src/node/3d/model.tsx
@@ -1,15 +1,18 @@
 import { useLoader } from "@react-three/fiber";
+import { useEffect, useState } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 interface IModel {
   url: string;
 }
 export const Model = ({ url }: IModel) => {
-    
-  const gltf = useLoader(GLTFLoader, '/test.glb');
-  console.log(gltf,'gltf')
-  return (
-    <>
-      <primitive object={gltf.scene} scale={0.5} />
-    </>
-  );
+  const [model, setModel] = useState<any>(null);
+  useEffect(() => {
+    const loader = new GLTFLoader();
+    loader.load(url, (gltf) => {
+      gltf.scene && setModel(gltf.scene);
+    });
+  }, [url]);
+  // const gltf = useLoader(GLTFLoader, 'http://10.180.5.186:30081/visualize_download//view/52460ab87d37442e899e4adb1deab7cb.glb');
+  // console.log(gltf,'gltf')
+  return <>{model ? <primitive object={model} scale={0.5} /> : <></>}</>;
 };
