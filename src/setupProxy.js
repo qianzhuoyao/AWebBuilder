8b9329efeae9ebbf274bf69cfb96b8695eb956dd
QaeJey <1059116864@qq.com>
2024-05-28 10:50:45 +0800

    remove

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-module.exports = function (app) {
-    app.use(createProxyMiddleware('/mwapi', {
-        target: 'http://10.180.5.186:30095',
-        changeOrigin: true,
-        pathRewrite: { '^/mwapi': '/mwapi' },
-    })
-    )
-}
